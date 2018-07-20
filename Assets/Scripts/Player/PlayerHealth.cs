84cef90bf88dcb480be6e8b2b63c92898f386d99
Daniel Everland <danieleverland@gmail.com>
2018-07-20 20:07:14 +0200

    Implemented player

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    [SerializeField]
+    private FloatReference _maxHealth;
+    [SerializeField]
+    private FloatReference _currentHealth;
+
+    private void Awake()
+    {
+        _currentHealth.Value = _maxHealth;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField]
+    private CharacterController _characterController;
+    [SerializeField]
+    private FloatReference _movementSpeed;
+
+    private void Update()
+    {
+        Vector2 input = PollInput();
+        Move(input);
+    }
+    private void Move(Vector2 direction)
+    {
+        _characterController.Move(direction * _movementSpeed * Time.deltaTime);
+    }
+    private Vector2 PollInput()
+    {
+        return new Vector2()
+        {
+            x = Input.GetAxisRaw("Horizontal"),
+            y = Input.GetAxisRaw("Vertical"),
+        }.normalized;
+    }
+}
