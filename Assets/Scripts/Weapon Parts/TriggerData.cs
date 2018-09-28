14d34f1643e1426b271ec637d3bdd98cbfa6071c
Daniel Everland <danieleverland@gmail.com>
2018-09-28 19:31:32 +0200

    Added some data to triggers

--- Assets/Scripts/Weapon Parts/TriggerData.cs
+++ Assets/Scripts/Weapon Parts/TriggerData.cs
@@ -5,5 +5,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Trigger.asset", menuName = MENU_ROOT + "Trigger", order = MENU_ORDER)]
 public class TriggerData : PartBase {
 
-	
+    public float Cooldown { get { return _cooldown; } }
+
+    [SerializeField, Range(0, 10)]
+    private float _cooldown = 0.3f;
 }
