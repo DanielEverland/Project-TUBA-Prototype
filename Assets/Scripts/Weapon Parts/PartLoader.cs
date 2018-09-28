4d8238389d7a0b04332153d2db9a875ac3aab1d9
Daniel Everland <danieleverland@gmail.com>
2018-09-28 17:46:40 +0200

    Added ability to easily load parts from Resources

---
+++ Assets/Scripts/Weapon Parts/PartLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Loads part assets from Resources into memory
+/// </summary>
+public static class PartLoader {
+
+	static PartLoader()
+    {
+        _triggerData = Resources.LoadAll<TriggerData>("").ToList();
+        _seekerData = Resources.LoadAll<SeekerData>("").ToList();
+        _eventData = Resources.LoadAll<EventData>("").ToList();
+    }
+
+    public static IEnumerable<TriggerData> TriggerData { get { return _triggerData; } }
+    public static IEnumerable<SeekerData> SeekerData { get { return _seekerData; } }
+    public static IEnumerable<EventData> EventData { get { return _eventData; } }
+
+    private static List<TriggerData> _triggerData;
+    private static List<SeekerData> _seekerData;
+    private static List<EventData> _eventData;
+}
