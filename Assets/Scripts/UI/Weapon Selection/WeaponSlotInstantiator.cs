3b615f47ac1aa092c487a6d3960216ccebc1e80c
Daniel Everland <danieleverland@gmail.com>
2018-09-28 20:34:27 +0200

    Implemented weapon slot count

---
+++ Assets/Scripts/UI/Weapon Selection/WeaponSlotInstantiator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSlotInstantiator : MonoBehaviour {
+
+    [SerializeField]
+    private IntReference _weaponSlotCount;
+    [SerializeField]
+    private GameObject _weaponSlotPrefab;
+    [SerializeField]
+    private Transform _parent;
+
+    private void Awake()
+    {
+        for (int i = 0; i < _weaponSlotCount.Value; i++)
+        {
+            GameObject instance = Instantiate(_weaponSlotPrefab);
+            instance.transform.SetParent(_parent);
+        }
+    }
+}
