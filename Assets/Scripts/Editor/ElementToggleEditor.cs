87349df192e1ce972bdb3c309e00ad8637b285fa
Daniel Everland <danieleverland@gmail.com>
2018-09-28 20:30:35 +0200

    Added element toggler to toggle UI elements

---
+++ Assets/Scripts/Editor/ElementToggleEditor.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.AnimatedValues;
+
+[CustomEditor(typeof(ElementToggle))]
+[CanEditMultipleObjects]
+public class ElementToggleEditor : Editor {
+
+    private SerializedProperty _targetProperty;
+    private SerializedProperty _autoSetStartState;
+    private SerializedProperty _startState;
+    private SerializedProperty _keyCode;
+
+    private AnimBool _startStateAnim;
+
+    private void OnEnable()
+    {
+        _targetProperty = serializedObject.FindProperty("_target");
+        _autoSetStartState = serializedObject.FindProperty("_autoSetStartState");
+        _startState = serializedObject.FindProperty("_startState");
+        _keyCode = serializedObject.FindProperty("_keyCode");
+
+        _startStateAnim = new AnimBool(_autoSetStartState.boolValue);
+        _startStateAnim.valueChanged.AddListener(Repaint);
+    }
+    public override void OnInspectorGUI()
+    {
+        //Target
+        EditorGUILayout.ObjectField(_targetProperty);
+
+        if(_targetProperty.objectReferenceValue == null)
+        {
+            EditorGUILayout.HelpBox("Target cannot be null", MessageType.Error);
+        }
+        else if(_targetProperty.objectReferenceValue == ((MonoBehaviour)target).gameObject)
+        {
+            EditorGUILayout.HelpBox("Target cannot be self\nThis will disallow key capture when target is disabled", MessageType.Warning);
+        }
+
+        //Keycode
+        EditorGUILayout.PropertyField(_keyCode);
+
+        //Auto-set Start State
+        EditorGUILayout.PropertyField(_autoSetStartState);
+        _startStateAnim.target = _autoSetStartState.boolValue;
+        
+        using (var group = new EditorGUILayout.FadeGroupScope(_startStateAnim.faded))
+        {
+            if (group.visible)
+            {
+                EditorGUI.indentLevel++;
+
+                EditorGUILayout.PropertyField(_startState);
+
+                EditorGUI.indentLevel--;
+            }
+        }
+        
+        serializedObject.ApplyModifiedProperties();        
+    }
+}
---
+++ Assets/Scripts/UI/Elements/ElementToggle.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementToggle : MonoBehaviour {
+    
+    [SerializeField]
+    private bool _autoSetStartState;
+    [SerializeField]
+    private bool _startState;
+    [SerializeField]
+    private GameObject _target;
+    [SerializeField]
+    private KeyCode _keyCode;
+    
+    private void Awake()
+    {
+        if (_autoSetStartState)
+            SetState(_startState);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyUp(_keyCode))
+        {
+            try
+            {
+                SetState(!_target.activeInHierarchy);
+            }
+            catch (System.NullReferenceException)
+            {
+                if (_target == null)
+                {
+                    throw new System.NullReferenceException("Element Toggle Error: Target cannot be null");
+                }
+
+                throw;
+            }
+        }
+    }
+    private void EnsureTargetIsNotSelf()
+    {
+        if(_target == gameObject)
+        {
+            Debug.LogError("Element Toggle Error: Target cannot be self. This will disallow event capture when disabled", this);
+        }
+    }
+    private void SetState(bool active)
+    {
+        try
+        {
+            _target.SetActive(active);
+        }
+        catch (System.NullReferenceException)
+        {
+            if(_target == null)
+            {
+                throw new System.NullReferenceException("Element Toggle Error: Target cannot be null");
+            }
+
+            throw;
+        }
+    }
+}
