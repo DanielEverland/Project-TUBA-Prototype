42440ef21df69f750eabd5516eef6617c04a7956
Daniel Everland <danieleverland@gmail.com>
2018-09-29 13:03:51 +0200

    Implemented UI so you can switch triggers

---
+++ Assets/Scripts/UI/Weapon Selection/TriggerSelector.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriggerSelector : MonoBehaviour {
+
+    [SerializeField]
+    private Dropdown _dropDown;
+
+    private WeaponSlotElement _owner;
+
+    public void Initialize(WeaponSlotElement owner)
+    {
+        _owner = owner;
+        
+        _dropDown.options = new List<Dropdown.OptionData>(PartLoader.TriggerData.Select(x => new Dropdown.OptionData(x.name)));
+        _dropDown.onValueChanged.AddListener(OnSelectionChanged);
+    }
+    private void OnSelectionChanged(int index)
+    {
+        _owner.ChangeTrigger(PartLoader.TriggerData.ElementAt(index));
+    }
+}
---
+++ Assets/Scripts/UI/Weapon Selection/WeaponSlotElement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSlotElement : MonoBehaviour {
+
+    [SerializeField]
+    private TriggerSelector _triggerSelector;
+
+    private void Awake()
+    {
+        _triggerSelector.Initialize(this);
+    }
+    public void ChangeTrigger(TriggerData trigger)
+    {
+        Debug.Log(trigger);
+    }
+}
