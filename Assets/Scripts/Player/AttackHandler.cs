73cf5ddcef657105825c1cae7478594d2b4ed734
Daniel Everland <danieleverland@gmail.com>
2018-09-29 13:05:32 +0200

    Renamed WeaponHandler to AttackHandler

--- Assets/Scripts/Player/AttackHandler.cs
+++ Assets/Scripts/Player/AttackHandler.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponHandler : MonoBehaviour {
+public class AttackHandler : MonoBehaviour {
 
     [SerializeField]
     private GameEvent OnFire;
