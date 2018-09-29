7dd1714aaafb7f2dc8a35ad9ae5e5a1b69a2bc60
Daniel Everland <danieleverland@gmail.com>
2018-09-29 13:21:34 +0200

    Backend for weapon handling

---
+++ Assets/Scripts/Player/WeaponHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WeaponHandler : MonoBehaviour {
+
+    [SerializeField]
+    private IntVariable _slotAmount;
+    
+    public IEnumerable<Weapon> Weapons { get { return _weapons; } }
+
+    private List<Weapon> _weapons;
+
+    private void Awake()
+    {
+        _weapons = new List<Weapon>();
+
+        for (int i = 0; i < _slotAmount.Value; i++)
+        {
+            _weapons.Add(new Weapon());
+        }
+    }
+    public void ChangeTrigger(int index, TriggerData trigger)
+    {
+        _weapons[index].TriggerData = trigger;
+
+        //_weaponsChanged.Raise();
+    }
+}
---
+++ Assets/Scripts/Weapon Parts/Weapon.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Contains all data for a given weapon
+/// </summary>
+public class Weapon : IEquatable<Weapon> {
+
+    public Weapon()
+    {
+        _guid = new Guid();
+    }
+
+    private Guid _guid;
+    
+    public TriggerData TriggerData
+    {
+        get
+        {
+            return _triggerData;
+        }
+        set
+        {
+            _triggerData = value;
+        }
+    }
+
+    private TriggerData _triggerData;
+
+    public bool Equals(Weapon other)
+    {
+        return other._guid == this._guid;
+    }
+    public override bool Equals(object obj)
+    {
+        if (obj == null)
+            return false;
+
+        if(obj is Weapon)
+        {
+            return Equals(obj as Weapon);
+        }
+
+        return false;
+    }
+    public override int GetHashCode()
+    {
+        return _guid.GetHashCode();
+    }
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
