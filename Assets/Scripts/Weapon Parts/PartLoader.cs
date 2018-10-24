9b1039ce43fe7dc99e2b51bfb0a9ae9535ae437c
Daniel Everland <danieleverland@gmail.com>
2018-10-24 17:48:10 +0200

    Logged parts loaded

--- Assets/Scripts/Weapon Parts/PartLoader.cs
+++ Assets/Scripts/Weapon Parts/PartLoader.cs
@@ -13,6 +13,8 @@ public static class PartLoader {
         _triggerData = Resources.LoadAll<TriggerData>("").ToList();
         _seekerData = Resources.LoadAll<SeekerData>("").ToList();
         _eventData = Resources.LoadAll<EventData>("").ToList();
+
+        LogData();
     }
 
     public static IEnumerable<TriggerData> TriggerData { get { return _triggerData; } }
@@ -22,4 +24,31 @@ public static class PartLoader {
     private static List<TriggerData> _triggerData;
     private static List<SeekerData> _seekerData;
     private static List<EventData> _eventData;
+
+    private static void LogData()
+    {
+        PrintInfo();
+        CheckWarnings();
+    }
+    private static void PrintInfo()
+    {
+        string infoString = string.Format(
+            "---Loading Parts---\n{0} triggers, {1} seekers, {2} events",
+            _triggerData.Count, _seekerData.Count, _eventData.Count);
+
+        Debug.Log(infoString);
+    }
+    private static void CheckWarnings()
+    {
+        WarnIfListIsEmpty(_triggerData, "Trigger Parts");
+        WarnIfListIsEmpty(_seekerData, "Seeker Parts");
+        WarnIfListIsEmpty(_eventData, "Event Parts");
+    }
+    private static void WarnIfListIsEmpty(ICollection collection, string collectionName)
+    {
+        if(collection.Count < 1)
+        {
+            Debug.LogError("No " + collectionName + " are loaded!");
+        }
+    }
 }
