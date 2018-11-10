7c9fb91d23b03250c143e6583473ae6dc1bf84fe
Daniel Everland <danieleverland@gmail.com>
2018-11-10 18:47:51 +0100

    Fixed issues with explosive force on player damaged

--- Assets/Scripts/Components/AddExplosiveForce.cs
+++ Assets/Scripts/Components/AddExplosiveForce.cs
@@ -10,18 +10,19 @@ public class AddExplosiveForce : MonoBehaviour {
     private FloatReference _force = new FloatReference(50);
     [SerializeField]
     private ForceMode2D _forceMode = ForceMode2D.Impulse;
+    [SerializeField]
+    private LayerMask _ignoreLayer;
 
+    protected LayerMask IgnoreLayer => _ignoreLayer;
     protected ForceMode2D ForceMode => _forceMode;
     protected float Radius => _radius.Value;
     protected float Force => _force.Value;
 
     public void AddForce()
     {
-        Debug.Log("addforce");
-
         foreach (RaycastHit2D hit in Physics2D.CircleCastAll(transform.position, Radius, Vector2.zero))
         {
-            if (hit.rigidbody == null)
+            if (hit.rigidbody == null || IgnoreLayer.value == (IgnoreLayer.value | 1 << hit.collider.gameObject.layer))
                 continue;
 
             Vector2 delta = hit.transform.position - transform.position;            
