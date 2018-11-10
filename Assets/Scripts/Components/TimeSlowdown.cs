ddec598e6a078d61bec6155de28c8a1dead3b82b
Daniel Everland <danieleverland@gmail.com>
2018-11-10 23:32:06 +0100

    Added some slowdown when player is damaged

---
+++ Assets/Scripts/Components/TimeSlowdown.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeSlowdown : MonoBehaviour {
+
+    [SerializeField]
+    private FloatReference _duration;
+    [SerializeField]
+    private AnimationCurve _curve = AnimationCurve.Constant(0, 1, 1);
+
+    protected float FullDuration => _duration.Value;
+    protected float SlowdownStart { get; set; }
+    protected AnimationCurve Curve => _curve;
+       
+    public virtual void Slowdown()
+    {
+        SlowdownStart = Time.unscaledTime;
+    }
+    protected virtual void Update()
+    {
+        float timeSinceSlowdown = Time.unscaledTime - SlowdownStart;
+
+        if(timeSinceSlowdown < FullDuration)
+        {
+            float durationPercentage = timeSinceSlowdown / FullDuration;
+
+            Time.timeScale = Curve.Evaluate(durationPercentage);
+        }
+    }
+}
