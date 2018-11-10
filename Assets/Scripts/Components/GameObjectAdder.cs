d440349c94fd3838cdb59adee7d44adff597f5cc
Daniel Everland <danieleverland@gmail.com>
2018-11-11 00:26:25 +0100

    Added all enemies to a collection

---
+++ Assets/Scripts/Components/GameObjectAdder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectAdder : MonoBehaviour {
+
+    [SerializeField]
+    private GameObjectCollection _targetCollection;
+
+    protected GameObjectCollection Collection => _targetCollection;
+
+	protected virtual void Awake()
+    {
+        Collection.Add(gameObject);
+    }
+    protected virtual void OnDestroy()
+    {
+        Collection.Remove(gameObject);
+    }
+}
