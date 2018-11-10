0ebd78506f9bc74da88a5f4e695fa138eb2786d0
Daniel Everland <danieleverland@gmail.com>
2018-11-10 16:30:05 +0100

    Implemented attacking on orb

---
+++ Assets/Scripts/Enemies/Combat/DamageOnTouch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageOnTouch : MonoBehaviour {
+
+    [SerializeField]
+    private FloatReference _damageAmount = new FloatReference(10);
+
+    protected float DamageAmount => _damageAmount.Value;
+
+    protected virtual void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.gameObject.GetComponentInChildren<Health>()?.TakeDamage(DamageAmount);
+    }
+}
