1ffd22b56db647b2677cb61e98dd443c3c9675f5
Daniel Everland <danieleverland@gmail.com>
2018-11-11 00:22:24 +0100

    Added camera follow script

---
+++ Assets/Scripts/Camera/CameraScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObjectReference _target;
+    [SerializeField]
+    private FloatReference _distance = new FloatReference(10);
+
+    protected GameObject Target => _target.Value;
+    protected float Distance => _distance.Value;
+
+    protected virtual void LateUpdate()
+    {
+        Vector3 targetPosition = Target.transform.position;
+        targetPosition.z = -Distance;
+
+        transform.position = targetPosition;
+    }
+}
