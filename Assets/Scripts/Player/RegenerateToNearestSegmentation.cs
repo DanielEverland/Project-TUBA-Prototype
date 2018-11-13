5a5a2a24ebfb908aa4959705537afbec3e3d81cf
Daniel Everland <danieleverland@gmail.com>
2018-11-13 20:01:56 +0100

    Added regenerative ability

---
+++ Assets/Scripts/Player/RegenerateToNearestSegmentation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegenerateToNearestSegmentation : MonoBehaviour
+{
+    [SerializeField]
+    private FloatReference _currentValue;
+    [SerializeField]
+    private FloatReference _valuePerSegmentation;
+    [SerializeField]
+    private GameEvent _onRegenerate;
+
+    protected float CurrentValue { get => _currentValue.Value; set => _currentValue.Value = value; }
+    protected float ValuePerSegmentation => _valuePerSegmentation.Value;
+    protected GameEvent OnRegenerate => _onRegenerate;
+    
+    public void Regenerate()
+    {
+        float targetValue = Utility.GetMaxValueForSegment(CurrentValue, ValuePerSegmentation);
+        
+        // If we're already at segment max, we want to get to the next segment max
+        if (targetValue == CurrentValue)
+            targetValue += ValuePerSegmentation;
+
+        CurrentValue = targetValue;
+        _onRegenerate?.Raise();
+    }
+}
---
+++ Assets/Scripts/Utility/Utility.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utility
+{
+    /// <summary>
+    /// Will return the max value of the current segment
+    /// </summary>
+    /// <param name="currentValue">Current value in total, not in relation to segment</param>
+    /// <param name="valuePerSegment">How many values does a segment hold?</param>
+    /// <returns>The max value of the current segment</returns>
+    public static float GetMaxValueForSegment(float currentValue, float valuePerSegment)
+    {
+        int currentSegment = Mathf.CeilToInt(currentValue / valuePerSegment);
+        return valuePerSegment * currentSegment;
+    }
+}
