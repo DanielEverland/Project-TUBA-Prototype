866714de47d2b52849b8b2e25cd98076e086e326
Daniel Everland <danieleverland@gmail.com>
2018-11-13 20:16:11 +0100

    Added lerping to healthbar

--- Assets/Scripts/UI/Player/PlayerHealthbar.cs
+++ Assets/Scripts/UI/Player/PlayerHealthbar.cs
@@ -14,6 +14,12 @@ public class PlayerHealthbar : MonoBehaviour {
     [SerializeField]
     private IntReference _segmentCount;
     [SerializeField]
+    private FloatReference _lerpSpeed = new FloatReference(10);
+    [SerializeField]
+    private BoolReference _lerpIncreases = new BoolReference(true);
+    [SerializeField]
+    private BoolReference _lerpDecreases = new BoolReference(true);
+    [SerializeField]
     private Image _segmentElement;
     [SerializeField]
     private Transform _layoutParent;
@@ -21,10 +27,16 @@ public class PlayerHealthbar : MonoBehaviour {
     protected Transform LayoutParent => _layoutParent;
     protected Image SegmentElement => _segmentElement;
     protected Image[] Segments { get; set; }
+    protected bool ShouldLerpIncreases => _lerpIncreases.Value;
+    protected bool ShouldLerpDecreases => _lerpDecreases.Value;
+    protected float LerpSpeed => _lerpSpeed.Value;
     protected float HealthPerSegment => _healthPerSegment.Value;
     protected float CurrentHealth => _currentHealth.Value;
     protected float MaxHealth => _maxHealth.Value;
     protected int SegmentCount => _segmentCount.Value;
+    protected float LerpValue { get; set; }
+    protected bool IsLerping { get; set; }
+    protected float PreviousHealth { get; set; }
 
     public virtual Image this[int index]
     {
@@ -36,18 +48,47 @@ public class PlayerHealthbar : MonoBehaviour {
     protected virtual void Awake()
     {
         SpawnSegments();
+        PollDifferences();
     }
     protected virtual void Update()
     {
+        PollDifferences();
+
+        Lerp();
         DrawSegments();
+
+        CacheHealth();
+    }
+    protected virtual void PollDifferences()
+    {
+        if(CurrentHealth != PreviousHealth)
+        {
+            IsLerping = false;
+
+            if (CurrentHealth > PreviousHealth && ShouldLerpIncreases)
+                IsLerping = true;
+
+            if (CurrentHealth < PreviousHealth && ShouldLerpDecreases)
+                IsLerping = true;
+        }
+    }
+    protected virtual void Lerp()
+    {
+        LerpValue = Mathf.Lerp(LerpValue, CurrentHealth, LerpSpeed * Time.deltaTime);
+    }
+    protected virtual void CacheHealth()
+    {
+        PreviousHealth = CurrentHealth;
     }
     protected virtual void DrawSegments()
     {
         for (int i = 0; i < Segments.Length; i++)
         {
+            float healthValue = IsLerping ? LerpValue : CurrentHealth;
+
             float minValue = HealthPerSegment * i;
             float maxValue = HealthPerSegment * (i + 1);
-            float percentageValue = Mathf.Clamp01((CurrentHealth - minValue) / (maxValue - minValue));
+            float percentageValue = Mathf.Clamp01((healthValue - minValue) / (maxValue - minValue));
 
             this[i].fillAmount = percentageValue;
         }
