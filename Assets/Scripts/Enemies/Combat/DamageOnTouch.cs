c780b23de5c7abfa12123c4057419ced14c4fd71
Daniel Everland <danieleverland@gmail.com>
2018-11-19 00:24:51 +0100

    Finished demon wraith attack

--- Assets/Scripts/Enemies/Combat/DamageOnTouch.cs
+++ Assets/Scripts/Enemies/Combat/DamageOnTouch.cs
@@ -8,13 +8,44 @@ public class DamageOnTouch : MonoBehaviour {
     private FloatReference _damageAmount = new FloatReference(10);
     [SerializeField]
     private LayerMask _ignorelayer;
+    [SerializeField]
+    private FloatReference _radius = new FloatReference(0.5f);
+    [SerializeField]
+    private BoolReference _destroyOnHit = new BoolReference(false);
+    [SerializeField]
+    private GameObject _destroyTarget;
 
+    protected GameObject DestroyTarget => _destroyTarget;
+    protected float Radius => _radius.Value;
     protected float DamageAmount => _damageAmount.Value;
     protected int LayerMask => _ignorelayer.value;
+    protected bool DestroyOnHit => _destroyOnHit.Value;
 
+    protected virtual void Update()
+    {
+        RaycastHit2D hit = Physics2D.CircleCast(transform.position, Radius, Vector2.zero);
+
+        if(hit)
+        {
+            if (LayerMask != (LayerMask | 1 << hit.collider.gameObject.layer))
+                Hit(hit.collider.gameObject);
+        }
+    }
     protected virtual void OnCollisionEnter2D(Collision2D collision)
     {
-        if(LayerMask != (LayerMask | 1 << collision.gameObject.layer))
-            collision.gameObject.GetComponentInChildren<Health>()?.TakeDamage(DamageAmount);
+        if (LayerMask != (LayerMask | 1 << collision.gameObject.layer))
+            Hit(collision.gameObject);
+    }
+    protected virtual void Hit(GameObject target)
+    {
+        target.GetComponentInChildren<Health>()?.TakeDamage(DamageAmount);
+
+        if (DestroyOnHit && DestroyTarget != null)
+            Destroy(DestroyTarget);
+    }
+    protected virtual void OnValidate()
+    {
+        if (DestroyTarget == null)
+            _destroyTarget = gameObject;
     }
 }
