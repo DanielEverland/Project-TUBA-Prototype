bca17e341248dc5be85e74b7eefc06e9eec1dcb7
Daniel Everland <danieleverland@gmail.com>
2018-11-19 14:29:40 +0100

    Added pattern spawner

---
+++ Assets/Scripts/Enemies/Combat/PatternSpawnerOnTarget.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatternSpawnerOnTarget : TargetAttacker
+{
+    [SerializeField]
+    private Pattern _pattern;
+    
+    protected Pattern Pattern => _pattern;
+
+    public override void Attack()
+    {
+        PatternObject obj = Pattern.Spawn();
+        obj.Parent.transform.position = Target.transform.position;
+    }
+}
