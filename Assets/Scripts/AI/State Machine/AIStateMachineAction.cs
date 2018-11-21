576d9014c5ac2fbd0a2be6046b94a033f3cc1d59
Daniel Everland <danieleverland@gmail.com>
2018-11-21 18:19:49 +0100

    Moved the only action implementation to its correct folder

--- Assets/Scripts/AI/State Machine/AIStateMachineAction.cs
+++ Assets/Scripts/AI/State Machine/AIStateMachineAction.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AIStateMachineAction : AIStateMachineObject
+public abstract class AIStateMachineAction : AIStateMachineObject
 {
     public virtual void Think() { }
     public virtual void Update() { }
