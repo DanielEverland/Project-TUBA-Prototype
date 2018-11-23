a4a8b855346bcf4de9089dec929184aeee79139d
Daniel Everland <danieleverland@gmail.com>
2018-11-23 12:25:42 +0100

    Removed old behaviour system

--- Assets/Scripts/AI/State Machine/AIAction.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class AIAction : AIStateMachineObject
-{
-    public virtual void Think(Agent agent) { }
-    public virtual void PerformAction(Agent agent) { }
-}
--- Assets/Scripts/AI/State Machine/AICondition.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class AICondition : AIStateMachineObject
-{
-    public abstract bool Evaluate(Agent agent);
-}
